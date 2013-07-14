bf55dafa1d27ce965a55f1fd126bc8d62ab8f538
Yohan Boniface <yb@enix.org>
2013-07-14 02:25:34 +0200

    Initial gh-pages commit

---
+++ legend.js
@@ -0,0 +1,54 @@
+var LEGEND = {
+    title: "HOT style",
+    description: "Humanitarian focused OSM base layer.",
+    entries: [{
+        title: 'Roads',
+        className: 'roads',
+        keys: [
+            {
+                coordinates: [19.67236, -72.11825, 15],
+                text: "Paved primary road"
+            },
+            {
+                coordinates: [19.70824, -72.41555, 15],
+                text: "highway=secondary"
+            },
+            {
+                coordinates: [19.72434, -72.16495, 15],
+                text: "highway=tertiary"
+            }
+        ]
+    },
+    {
+        title: 'Landuse',
+        className: 'landuse',
+        keys: [
+            {
+                coordinates: [19.7365, -72.1487, 16],
+                text: "wetland"
+            },
+            {
+                coordinates: [19.71656, -72.12472, 17],
+                text: "farm land / agriculture"
+            },
+            {
+                coordinates: [19.66952, -72.00810, 17],
+                text: "industrial"
+            }
+        ]
+    },
+    {
+        title: 'Health',
+        className: 'poi',
+        keys: [
+            {
+                coordinates: [19.67108, -72.12233, 19],
+                text: "hospital"
+            },
+            {
+                coordinates: [19.67269, -72.12493, 19],
+                text: "pharmacy"
+            }
+        ]
+    }
+]};
