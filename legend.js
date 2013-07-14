3a0a18249fe99889638f1aeffc8ebf753e34d3f1
Yohan Boniface <yb@enix.org>
2013-07-14 13:11:56 +0200

    More on legend (cf #44)

--- legend.js
+++ legend.js
@@ -16,6 +16,22 @@ var LEGEND = {
             {
                 coordinates: [19.72434, -72.16495, 15],
                 text: "highway=tertiary"
+            },
+            {
+                coordinates: [19.66985, -72.12450, 19],
+                text: "residential (paved)"
+            },
+            {
+                coordinates: [19.67993, -72.15773, 16],
+                text: "Unpaved road"
+            },
+            {
+                coordinates: [19.67921, -72.14910, 17],
+                text: "Track"
+            },
+            {
+                coordinates: [19.67885, -72.16084, 17],
+                text: "Path"
             }
         ]
     },
@@ -38,16 +54,106 @@ var LEGEND = {
         ]
     },
     {
-        title: 'Health',
+        title: 'Health & education',
         className: 'poi',
         keys: [
             {
                 coordinates: [19.67108, -72.12233, 19],
-                text: "hospital"
+                text: "Hospital"
             },
             {
                 coordinates: [19.67269, -72.12493, 19],
-                text: "pharmacy"
+                text: "Pharmacy"
+            },
+            {
+                coordinates: [19.65555, -72.07040, 17],
+                text: "University"
+            },
+            {
+                coordinates: [19.54809, -71.72016, 19],
+                text: "School"
+            },
+            {
+                coordinates: [19.55134, -71.72704, 19],
+                text: "Kindergarden"
+            }
+        ]
+    },
+    {
+        title: 'Money',
+        className: 'poi',
+        keys: [
+            {
+                coordinates: [19.75792, -72.20032, 20],
+                text: "bank"
+            },
+            {
+                coordinates: [19.55020, -71.72397, 20],
+                text: "atm"
+            },
+            {
+                coordinates: [19.10882, -72.69989, 20],
+                text: "bureau de change"
+            },
+            {
+                coordinates: [19.55042, -71.72563, 20],
+                text: "money transfer"
+            }
+        ]
+    },
+    {
+        title: 'Shops',
+        className: 'poi',
+        keys: [
+            {
+                coordinates: [19.55006, -71.72539, 20],
+                text: "Generic shop icon"
+            },
+            {
+                coordinates: [19.55053, -71.72614, 20],
+                text: "Hardware"
+            },
+            {
+                coordinates: [19.55093, -71.72694, 20],
+                text: "Clothes"
+            },
+            {
+                coordinates: [19.55010, -71.72416, 20],
+                text: "Dry cleaning"
+            },
+            {
+                coordinates: [19.66933, -72.12373, 20],
+                text: "hairdresser"
+            }
+        ]
+    },
+    {
+        title: 'Food & drinks',
+        className: 'poi',
+        keys: [
+            {
+                coordinates: [19.67031, -72.12675, 20],
+                text: "Restaurant"
+            },
+            {
+                coordinates: [19.66781, -72.12224, 20],
+                text: "Bakery"
+            },
+            {
+                coordinates: [19.66910, -72.12724, 20],
+                text: "Bar"
+            },
+            {
+                coordinates: [19.67214, -72.12361, 20],
+                text: "Beverages"
+            },
+            {
+                coordinates: [19.69580, -72.15649, 20],
+                text: "Café"
+            },
+            {
+                coordinates: [19.75773, -70.51684, 20],
+                text: "Pub"
             }
         ]
     }
